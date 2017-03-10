aa17936fc032f75618410c48473ebecd1997d11d
David Renner <arenner38@yahoo.com>
2017-03-10 13:26:53 -0600

    Added layout file and linked it up to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,19 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title> </title>
-    </head>
-    <body>
+@extends ('layout')
 
-        <h1>
-           <?= $name ?>'s Laravel Page. And he's <?= (string)$age ?>. Just sayin'...
-        </h1>
+@section ('content')
+    <h1>
+       <?= $name ?>'s Laravel Page. And he's <?= (string)$age ?>. Just sayin...
+    </h1>
 
-        <ul>
-            <?php foreach ($phpinfo as $info) : ?>
-                <li><?= $info ?></li>
-            <?php endforeach ?>
-        </ul>
+    <ul>
+        <?php foreach ($phpinfo as $info) : ?>
+            <li><?= $info ?></li>
+        <?php endforeach ?>
+    </ul>
 
-        <ul>
-            @foreach ($tests as $test)
-                <li>{{ $test->info }}</li>
-            @endforeach
-        </ul>
-
-    </body>
-</html>
+    <ul>
+        @foreach ($tests as $test)
+            <li>{{ $test->info }}</li>
+        @endforeach
+    </ul>
+@endsection
